5239ebfad56bc636d24d85d0b7230cedc5c5782e
Bhuvanesh24 <132251713+Bhuvanesh24@users.noreply.github.com>
2023-08-24 20:01:15 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,10 @@
+/*function setActive(index) {
+  var buttons = document.querySelectorAll('.nav-list .button');
+  buttons.forEach(function(button, i) {
+      if (i === index) {
+          button.classList.add('active');
+      } else {
+          button.classList.remove('active');
+      }
+  });
+}*/
